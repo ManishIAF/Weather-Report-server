ad0ca899a06f85c58d4f23a23f0bdc71ad3841da
Manish Shaw <shawmanish2580@gmail.com>
2023-09-09 11:04:17 +0530

    uploading weather report server

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
+import https from 'https'
+
+const app = express()
+
+app.use(cors({origin:'http://localhost:3000'}))
+
+app.get('/',(req,res)=>res.status(200).send('home'))
+
+app.get('/api/weatherReport',(req,res)=>{
+    try {
+    
+        const {city} = req.query
+
+        if(!city) return res.status(404).send('city name is required')
+
+        const unit = "metric";
+
+        const url = `${process.env.WEATHER_URI}?q=`+ city +"&units="+unit+"&appid="+process.env.WEATHER_API_KEY;
+        
+        https.get(url , (response)=>{
+            
+            response.on("data", async(data)=>{
+            const weatherData = await JSON.parse(data);
+            res.status(200).send(weatherData)
+            });   
+
+        });
+        
+    } catch (error) {
+
+        res.status(500).send('server error')
+        
+    }
+    
+})
+
+app.listen(8000,()=>{
+    console.log(`server started at http://localhost:${8000}`);
+})
